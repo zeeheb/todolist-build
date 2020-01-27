78009800cf11074e6cd3b025fc36043900566194
José Eduardo Brandão <39067212+zeeheb@users.noreply.github.com>
2020-01-27 17:26:44 -0300

    ready

---
+++ precache-manifest.67d681612cc20263b635a1ae7f18adf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456a8db7f2ff830621bb70f883c51f60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebdaab2cc9c03e631f5f",
+    "url": "/static/css/main.0d32ea03.chunk.css"
+  },
+  {
+    "revision": "9db19fbbb5694f977604",
+    "url": "/static/js/2.4b5e7f8f.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.4b5e7f8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebdaab2cc9c03e631f5f",
+    "url": "/static/js/main.7f1d08f1.chunk.js"
+  },
+  {
+    "revision": "fbdc00db4a1e5ef37310",
+    "url": "/static/js/runtime-main.f944cc14.js"
+  }
+]);
